8e4164d788ee45de4282114a26cc948dca1cf3af
Glowupmatt <glowupmatt@gmail.com>
2024-10-11 16:01:05 -0700

    understand spring JDBC

--- src/main/java/com/matthew/learn_jpa_hibernate/course/jdbc/CourseJdbcCommandLineRunner.java
+++ src/main/java/com/matthew/learn_jpa_hibernate/course/jdbc/CourseJdbcCommandLineRunner.java
@@ -4,6 +4,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
+import com.matthew.learn_jpa_hibernate.course.Course;
+
+
 @Component
 public class CourseJdbcCommandLineRunner implements CommandLineRunner {
     @Autowired
@@ -11,7 +14,14 @@ public class CourseJdbcCommandLineRunner implements CommandLineRunner {
     
     @Override
     public void run(String... args) throws Exception {
-        repository.insert();
+        repository.insert(new Course(1, "Learn AWS Now!", "in28minutes"));
+        repository.insert(new Course(2, "Learn AWS Nowfdsfdsa!", "in28minutes"));
+        repository.insert(new Course(3, "Learn AWS Nowfdsfds!", "in28minutes"));
+        
+        repository.deleteById(1);
+
+        System.out.println(repository.findById(2));
+        System.out.println(repository.findById(3));
     }
 
 }
--- src/main/java/com/matthew/learn_jpa_hibernate/course/jdbc/CourseJdbcRepository.java
+++ src/main/java/com/matthew/learn_jpa_hibernate/course/jdbc/CourseJdbcRepository.java
@@ -1,8 +1,12 @@
 package com.matthew.learn_jpa_hibernate.course.jdbc;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
+
+import com.matthew.learn_jpa_hibernate.course.Course;
+
 
 @Repository
 public class CourseJdbcRepository {
@@ -12,11 +16,28 @@ public class CourseJdbcRepository {
     private static String INSERT_QUERY=
     """
         insert into course (id, name, author)     
-        values(1, 'Learn AWS', 'in38Minutes');   
+        values(?, ?, ?);   
+    """;
+    private static String DELETE_QUERY=
+    """
+        delete from course    
+        where id = ?  
+    """;
+    private static String SELECT_QUERY=
+    """
+        select * from course    
+        where id = ?  
     """;
 
-    public void insert() {
-        springJdbcTemplate.update(INSERT_QUERY);
+    public void insert(Course course) {
+        springJdbcTemplate.update(INSERT_QUERY, course.getId(), course.getName(), course.getAuthor());
+    }
+    public void deleteById(long id) {
+        springJdbcTemplate.update(DELETE_QUERY, id);
+    }
+    public Course findById(long id) {
+       return springJdbcTemplate.queryForObject(SELECT_QUERY,
+                new BeanPropertyRowMapper<>(Course.class), id);
     }
 
 
